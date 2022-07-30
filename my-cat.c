f95f5a1bbb3175c996e17ac1d8e8282f55e0122d
Marno <thejavasaurus@gmail.com>
2022-07-30 23:50:24 +0300

    adding comments

--- my-cat.c
+++ my-cat.c
@@ -4,28 +4,28 @@
 
 int main(int argc, char **argv)
 {
-    FILE *fp;
-    const int bufferSize = 4096;
-    char buffer[bufferSize];
-    int currentFile = (argc > 1 ? 1 : 0);
+    FILE *fp; // tiedosto pointteri
+    const int bufferSize = 4096; // bufferin koko
+    char buffer[bufferSize]; // bufferi
+    int currentFile = (argc > 1 ? 1 : 0); // muuttuja, jolla kerrotaan monta tiedostoa luetaan, jollei yhtään anneta laitetaan muuttujan arvoksi 0
 
-    if (argc < 1) {
+    if (argc < 1) { // ei mitään catattavaa täällä..
         exit(0);
     } else {
-        while (currentFile < argc) { 
-            fp = fopen(argv[argc -1], "rb");
+        while (currentFile < argc) { // luupataan kunnes parametrien määrä on pienempi kuin currentFile muuttujan arvo
+            fp = fopen(argv[argc -1], "rb"); // avataan tiedosto luettavaksi flägillä: read binary
         
-            if(fp == NULL) {
+            if(fp == NULL) { // tiedoston avaaminen epäonnistui
                 fprintf(stderr, "my-cat: cannot open file\n");
                 exit(1);
             }
-            while (fgets(buffer, bufferSize, (fp == NULL ? stdin : fp))) {
-                int length = strlen(buffer);
-                buffer[length -1] = '\0';
-                fprintf(stdout, "%s\n", buffer);
+            while (fgets(buffer, bufferSize, (fp == NULL ? stdin : fp))) { // luetaan tiedoston sisältö tarkastamalla, ettei fp ole null ternary-tavalla
+                int length = strlen(buffer); // katsotaan bufferin koko
+                buffer[length -1] = '\0'; // viimeiseksi merkiksi null terminator
+                fprintf(stdout, "%s\n", buffer); // tulostetaan tiedoston sisältö
             }
 
-            fclose(fp);
+            fclose(fp); // suljetaan tiedosto
             currentFile++;
         }
     }
